959d0ba152ad396ddcc8961886e61e9c454fc9b3
ChinCluBi <chincub@gmail.com>
2015-06-22 13:18:07 +0700

    authenticate redirect success

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen(8000);
+connect()
+// .use(function(req, res){
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000/");
+//     res.end(connect.static(__dirname));
+//     console.log('xxxxx');
+//  })
+.use(serveStatic(__dirname))
+.listen(8000);
